2191d9c3e12023d75d1e96d2a12e829ee89b28c2
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 10:56:03 -0500

    add more icons

--- app/js/app.js
+++ app/js/app.js
@@ -18,6 +18,8 @@
             .icon('account-multiple-plus', 'img/icons/account-multiple-plus.svg', 24)
             .icon('account', 'img/icons/account.svg', 24)
             .icon('facebook', 'img/icons/facebook.svg', 24)
+            .icon('menu', 'img/icons/menu.svg', 24)
+            .icon('more_vert', 'img/icons/more_vert.svg', 24)
             .icon('key', 'img/icons/key.svg', 24);
 
     };
