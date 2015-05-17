1578eae7c7a78562945d974d9131ae917ef806dc
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 05:31:13 -0500

    declare SingupController

---
+++ app/js/auth/controllers/singup.controller.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('exsharer.auth')
+        .controller('SingupController', SingupController);
+
+    function SingupController(){
+
+    };
+})();
