2f6dba08b32d540ae980d7c434d0b5724e84ad94
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 10:10:41 -0500

    add fb icon

--- app/js/app.js
+++ app/js/app.js
@@ -11,12 +11,13 @@
     .run(run);
 
     function config($mdIconProvider, $locationProvider){
-        
+
         //icons
         $mdIconProvider
             .icon('account-plus', 'img/icons/account-plus.svg', 24)
             .icon('account-multiple-plus', 'img/icons/account-multiple-plus.svg', 24)
             .icon('account', 'img/icons/account.svg', 24)
+            .icon('facebook', 'img/icons/facebook.svg', 24)
             .icon('key', 'img/icons/key.svg', 24);
 
     };
