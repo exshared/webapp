4da4dcd8c19a61bb2400795010486631fd902813
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 05:31:38 -0500

    add a botonsheet and dialogs with login and registry

--- app/js/components/controllers/exMenu.controller.js
+++ app/js/components/controllers/exMenu.controller.js
@@ -3,9 +3,56 @@
 
     angular
         .module('exsharer.components')
-        .controller('ExMenuController', ExMenuController);
+        .controller('ExMenuController', ExMenuController)
+        .controller('OptionsController', OptionsController);
 
-    function ExMenuController(){
+    function ExMenuController($scope, $mdBottomSheet, Auth){
 
+        $scope.options = function($event){
+
+            var base = 'views/components/options-';
+            var template = Auth.userLogged() ? base + 'auth' : base + 'no-auth';
+            template = template + '.tpl.html';
+
+            $mdBottomSheet.show({
+                templateUrl: 'views/components/options-no-auth.tpl.html',
+                controller: 'OptionsController',
+                targetEvent: $event
+            });
+        };
+    };
+
+    function OptionsController($scope, $mdDialog, $mdBottomSheet){
+
+        $scope.items = [
+            { name: 'Iniciar Sesión', icon: 'account' },
+            { name: 'Registrarse', icon: 'account-multiple-plus' }
+        ];
+
+        $scope.action = function(index, $event){
+
+            $mdBottomSheet.hide()
+
+            if(index == 0){
+
+                $mdDialog.show({
+                    controller: 'LoginController',
+                    templateUrl: 'views/auth/login.tpl.html',
+                    clickOutsideToClose: true,
+                    focusOnOpen: true,
+                    targetEvent : $event
+                });
+            }
+            if(index == 1){
+
+                $mdDialog.show({
+                    controller: 'SingupController',
+                    templateUrl: 'views/auth/singup.tpl.html',
+                    clickOutsideToClose: true,
+                    focusOnOpen: true,
+                    targetEvent : $event
+                });
+            }
+        }
     };
 })();
