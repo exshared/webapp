9c35c54609b4f49db82609bfc53a98212e40999f
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 03:04:10 -0500

    declare the auth module

---
+++ app/js/auth/module.js
@@ -0,0 +1,17 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('exsharer.auth', ['ui.router']);
+        .config(config)
+        .run(run);
+
+    function config(){
+
+    };
+
+    function run(){
+
+    };
+    
+})();
