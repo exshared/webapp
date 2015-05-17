02894bc02ff6e65e168d740e3f823e66e177f929
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 05:31:47 -0500

    declare some icons

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,14 @@
     .config(config)
     .run(run);
 
-    function config($stateProvider){
+    function config($stateProvider, $mdIconProvider){
+
+        //icons
+
+        $mdIconProvider
+            .icon('account-plus', 'img/icons/account-plus.svg', 24)
+            .icon('account-multiple-plus', 'img/icons/account-multiple-plus.svg', 24)
+            .icon('account', 'img/icons/account.svg', 24);
 
         $stateProvider
 
