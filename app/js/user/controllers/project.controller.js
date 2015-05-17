430cadcb0057e4420c3b1604ebd81fc45676242b
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 09:42:14 -0500

    declare project controller

---
+++ app/js/user/controllers/project.controller.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('exsharer.user')
+        .controller('ProjectController', ProjectController);
+
+    function ProjectController(){
+
+    };
+})()
