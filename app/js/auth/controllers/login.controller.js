020da428eadce1449973c5f833ff53ee934a9966
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 05:31:04 -0500

    declare login controller

---
+++ app/js/auth/controllers/login.controller.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('exsharer.auth')
+        .controller('LoginController', LoginController);
+
+    function LoginController(){
+        
+    };
+
+})();
