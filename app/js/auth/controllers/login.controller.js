74fe1be1889d7e7642d06520555d71b851b15fcf
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 09:09:21 -0500

    login function

--- app/js/auth/controllers/login.controller.js
+++ app/js/auth/controllers/login.controller.js
@@ -5,8 +5,13 @@
         .module('exsharer.auth')
         .controller('LoginController', LoginController);
 
-    function LoginController(){
-        
+    function LoginController($scope, Auth){
+
+        $scope.login = function(user){
+            Auth.password(user.username, user.password).then(function(data){
+                console.log(data);
+            });
+        };
     };
 
 })();
