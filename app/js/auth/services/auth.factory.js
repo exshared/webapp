115e920950632656be98e2ba6adeac658907aa5a
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 09:09:28 -0500

    changes

--- app/js/auth/services/auth.factory.js
+++ app/js/auth/services/auth.factory.js
@@ -24,16 +24,12 @@
             },
 
             password: function(username, password){
-                var client = this.clientLogged(),
-                            tokenType = client.token_type,
-                            token = client.access_token;
 
                 return $http({
                     method: 'post',
                     url: api.token,
                     headers: {
                         'Content-Type': headers.urlencoded,
-                        Authorization: headers.authorization(tokenType, token)
                     },
                     data: $.param(access.password(username, password)),
                 }).then(function(response){
