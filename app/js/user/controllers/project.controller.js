f263dac8205cc36d61b6041203b88ca9a6949815
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 13:38:30 -0500

    open bottom sheet

--- app/js/user/controllers/project.controller.js
+++ app/js/user/controllers/project.controller.js
@@ -5,15 +5,26 @@
         .module('exsharer.user')
         .controller('ProjectController', ProjectController);
 
-    function ProjectController($scope, $state, Project){
+    function ProjectController($scope, $state, Project, $mdBottomSheet){
 
         var project = $state.params.id;
 
         $scope.project = {};
+        $scope.fab = fab;
 
         getProject();
 
+        function fab($event){
+
+            $mdBottomSheet.show({
+                templateUrl: 'views/user/project-options.tpl.html',
+                controller: 'ProjectOptionsController',
+                targetEvent: $event
+            });
+        };
+
         function getProject(){
+            
             Project.get(project).then(function(data){
                 $scope.project = data;
                 console.log(data);
