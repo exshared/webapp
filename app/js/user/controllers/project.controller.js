f7fd7cbd53cda1dd2d9863efa5f68465bf05c521
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-17 12:20:00 -0500

    get project

--- app/js/user/controllers/project.controller.js
+++ app/js/user/controllers/project.controller.js
@@ -5,7 +5,20 @@
         .module('exsharer.user')
         .controller('ProjectController', ProjectController);
 
-    function ProjectController(){
-        
+    function ProjectController($scope, $state, Project){
+
+        var project = $state.params.id;
+
+        $scope.project = {};
+
+        getProject();
+
+        function getProject(){
+            Project.get(project).then(function(data){
+                $scope.project = data;
+                console.log(data);
+            });
+        }
+
     };
 })()
